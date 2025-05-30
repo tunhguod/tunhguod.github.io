39f6490930e16997e669cad512c125ab92fa0d10
tunhguod <127116759+tunhguod@users.noreply.github.com>
2025-05-31 04:24:44 +0900

    Update script.js

--- HRFlagChk/script/script.js
+++ HRFlagChk/script/script.js
@@ -3,26 +3,26 @@ const flagMap = [
    A  B  C  D  E  F  G  H  H' I  J  K  L  L' M  N  O  P  Q  R  S  T  U  V  W
   **/
   [0, 0, 1, 0, 0, 3, 0, 0, 1, 1, 4, 0, 0, 1, 2, 3, 0, 3, 4, 2, 0, 4, 0, 1, 0],
-  [1, 4, 2, 4, 4, 1, 3, 4, 2, 2, 0, 1, 4, 2, 3, 5, 3, 4, 3, 3, 5, 3, 1, 5, 1],
-  [2, 2, 3, 2, 2, 2, 4, 2, 3, 3, 1, 0, 4, 3, 2, 5, 2, 2, 2, 2, 5, 2, 5, 3, 2],
-  [3, 3, 4, 3, 3, 3, 3, 3, 4, 4, 2, 1, 4, 4, 3, 5, 5, 3, 0, 5, 5, 0, 5, 5, 3],
-  [0, 0, 4, 4, 4, 0, 4, 0, 2, 2, 3, 2, 4, 2, 4, 4, 5, 5, 5, 1, 5, 5, 4, 4, 4],
-  [1, 1, 1, 1, 0, 1, 0, 1, 3, 3, 2, 3, 1, 3, 5, 4, 0, 4, 5, 0, 5, 5, 1, 1, 0],
+  [1, 4, 2, 4, 4, 1, 3, 4, 2, 2, 0, 1, 4, 2, 3, 4, 3, 4, 3, 3, 1, 3, 1, 1, 1],
+  [2, 2, 3, 2, 2, 2, 4, 2, 3, 3, 1, 0, 4, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2],
+  [3, 3, 4, 3, 3, 3, 3, 3, 4, 4, 2, 1, 4, 4, 3, 3, 3, 3, 0, 3, 3, 0, 3, 3, 3],
+  [0, 0, 4, 4, 4, 0, 4, 0, 2, 2, 3, 2, 4, 2, 4, 4, 1, 4, 1, 1, 4, 1, 4, 4, 4],
+  [1, 1, 1, 1, 0, 1, 0, 1, 3, 3, 2, 3, 1, 3, 0, 4, 0, 4, 2, 0, 4, 2, 1, 1, 0],
   [2, 0, 2, 1, 1, 2, 1, 0, 0, 0, 3, 4, 0, 0, 1, 2, 1, 2, 3, 1, 2, 3, 0, 0, 2],
-  [0, 2, 1, 0, 0, 3, 2, 3, 1, 1, 4, 2, 3, 1, 0, 0, 2, 5, 4, 2, 0, 4, 1, 1, 0],
-  [1, 0, 0, 0, 1, 0, 0, 0, 4, 2, 3, 3, 0, 1, 3, 5, 2, 1, 3, 3, 1, 3, 0, 0, 0],
-  [2, 1, 1, 1, 2, 1, 1, 1, 1, 3, 0, 4, 1, 2, 2, 2, 5, 5, 3, 3, 5, 3, 1, 1, 2],
-  [3, 2, 2, 2, 3, 2, 2, 2, 2, 4, 1, 0, 2, 3, 5, 3, 5, 5, 2, 2, 3, 2, 2, 5, 3],
+  [0, 2, 1, 0, 0, 3, 2, 3, 1, 1, 4, 2, 3, 1, 0, 0, 2, 0, 4, 2, 0, 4, 1, 1, 0],
+  [1, 0, 0, 0, 1, 0, 0, 0, 4, 2, 3, 3, 0, 1, 3, 1, 2, 1, 3, 3, 1, 3, 0, 0, 0],
+  [2, 1, 1, 1, 2, 1, 1, 1, 1, 3, 0, 4, 1, 2, 2, 2, 3, 2, 3, 3, 2, 3, 1, 1, 2],
+  [3, 2, 2, 2, 3, 2, 2, 2, 2, 4, 1, 0, 2, 3, 3, 3, 4, 3, 2, 2, 3, 2, 2, 2, 3],
   [4, 3, 3, 3, 4, 3, 3, 3, 3, 1, 0, 1, 3, 4, 4, 4, 3, 4, 0, 3, 4, 0, 3, 3, 3],
   [0, 4, 4, 4, 0, 0, 0, 4, 0, 2, 3, 2, 4, 0, 4, 3, 1, 3, 1, 1, 3, 1, 0, 0, 0],
-  [0, 1, 0, 1, 0, 1, 1, 1, 3, 3, 2, 3, 1, 1, 4, 5, 5, 0, 2, 2, 5, 2, 1, 5, 0],
-  [2, 0, 0, 2, 2, 2, 2, 2, 4, 4, 3, 4, 1, 2, 3, 5, 3, 1, 3, 3, 1, 3, 2, 2, 2],
-  [2, 1, 2, 1, 2, 1, 3, 1, 0, 0, 0, 4, 3, 3, 5, 2, 4, 5, 0, 0, 5, 0, 5, 3, 3],
-  [4, 2, 2, 2, 3, 2, 4, 2, 1, 1, 1, 0, 4, 4, 5, 5, 1, 5, 1, 1, 3, 1, 4, 4, 3],
+  [0, 1, 0, 1, 0, 1, 1, 1, 3, 3, 2, 3, 1, 1, 4, 0, 2, 0, 2, 2, 0, 2, 1, 1, 0],
+  [2, 0, 0, 2, 2, 2, 2, 2, 4, 4, 3, 4, 1, 2, 3, 1, 3, 1, 3, 3, 1, 3, 2, 2, 2],
+  [2, 1, 2, 1, 2, 1, 3, 1, 0, 0, 0, 4, 3, 3, 0, 2, 4, 2, 0, 0, 2, 0, 3, 3, 3],
+  [4, 2, 2, 2, 3, 2, 4, 2, 1, 1, 1, 0, 4, 4, 1, 3, 1, 3, 1, 1, 3, 1, 4, 4, 3],
   [4, 3, 3, 3, 4, 3, 4, 3, 2, 2, 0, 1, 4, 2, 2, 4, 2, 4, 2, 2, 4, 2, 3, 3, 3],
-  [0, 0, 0, 4, 0, 0, 0, 4, 3, 3, 3, 2, 4, 3, 3, 0, 5, 5, 5, 3, 5, 5, 0, 0, 0],
-  [1, 0, 1, 0, 0, 1, 0, 0, 4, 4, 4, 3, 1, 4, 0, 1, 0, 1, 5, 0, 5, 5, 0, 0, 0],
-  [0, 0, 1, 2, 0, 0, 2, 0, 0, 0, 3, 4, 1, 0, 1, 2, 5, 2, 5, 1, 5, 5, 0, 5, 0],
+  [0, 0, 0, 4, 0, 0, 0, 4, 3, 3, 3, 2, 4, 3, 3, 0, 3, 0, 1, 3, 0, 1, 0, 0, 0],
+  [1, 0, 1, 0, 0, 1, 0, 0, 4, 4, 4, 3, 1, 4, 0, 1, 0, 1, 2, 0, 1, 2, 0, 0, 0],
+  [0, 0, 1, 2, 0, 0, 2, 0, 0, 0, 3, 4, 1, 0, 1, 2, 1, 2, 3, 1, 2, 3, 0, 0, 0],
 ]
 
 const prizeMap = [
@@ -100,8 +100,7 @@ let xValue = 0;
 
 const bbFlagDisplay = document.getElementById('info1');
 const rbFlagDisplay = document.getElementById('info2');
-const tbcFlagDisplay = document.getElementById('info3');
-const prizeDisplay = document.getElementById('info4');
+const prizeDisplay = document.getElementById('info3');
 
 let startY = 0;
 let isDragging = false;
@@ -165,7 +164,6 @@ function getFlagNamesStr(reelIdx, xValue) {
   const flagMapRowData = flagMap[arrIdx];
   const findBbFlagIdx = [];
   const findRbFlagIdx = [];
-  const findTbcFlagIdx = [];
 
   for (let i = 0; i < flagMapRowData.length; i++) {
     let flagName = flagNames[i];
@@ -175,12 +173,10 @@ function getFlagNamesStr(reelIdx, xValue) {
       } else {
         findRbFlagIdx.push(flagName);
       }
-    } else if (flagMapRowData[i] === 5) {
-      findTbcFlagIdx.push(flagName);
     }
   }
 
-  return [findBbFlagIdx, findRbFlagIdx, findTbcFlagIdx];
+  return [findBbFlagIdx, findRbFlagIdx];
 }
 
 function getPrizeNamesStr(reelIdx, xValue) {
@@ -212,7 +208,6 @@ function updateDisplay() {
   const findData = getFlagNamesStr(stopIndex, xValue);
   const findBbFlagIdx = findData[0];
   const findRbFlagIdx = findData[1];
-  const findTbcFlagIdx = findData[2];
   const findPrizeIdx = getPrizeNamesStr(stopIndex, xValue);
   if (findBbFlagIdx.length > 0) {
     bbFlagDisplay.textContent = findBbFlagIdx.join(", ");
@@ -223,11 +218,6 @@ function updateDisplay() {
     rbFlagDisplay.textContent = findRbFlagIdx.join(", ");
   } else {
     rbFlagDisplay.textContent = "なし";
-  }
-  if (findTbcFlagIdx.length > 0) {
-    tbcFlagDisplay.textContent = findTbcFlagIdx.join(", ");
-  } else {
-    tbcFlagDisplay.textContent = "なし (済)";
   }
   if (findPrizeIdx.length > 0) {
     prizeDisplay.textContent = findPrizeIdx.join(", ");
